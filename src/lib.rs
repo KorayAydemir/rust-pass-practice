af7d68eb4be69cf012251bb6aec6787b5ea62401
Koray Aydemir <korayaydemir276@gmail.com>
2023-02-14 19:31:40 +0300

    feat: enter and save password

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,28 @@
-use std::io::{self, Result};
+use std::{
+    fs::File,
+    io::{self, stdout, Result, Write},
+};
 
 #[cfg(test)]
 mod tests {
     #[test]
-    fn check_pass() {}
+    fn check_pass_test() {
+        let saved_pass = "koray_123!*";
+        let entered_pass = "koray_123!*";
+
+        assert_eq!(check_pass(&saved_pass, &entered_pass), true);
+    }
 }
 
-fn get_pass() -> Result<String> {
+pub fn save_pass(entered_pass: &str) -> Result<&str> {
+    let mut file = File::create("saved_pass.txt")?;
+    file.write_all(entered_pass.as_bytes())?;
+    Ok("your password has been saved !")
+}
+
+pub fn get_pass() -> Result<String> {
     let mut input = String::new();
     io::stdin().read_line(&mut input)?;
+    let _ = stdout().flush();
     Ok(input)
 }
-
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use rust_pass_practice::get_pass;
+use rust_pass_practice::save_pass;
+
 fn main() {
-    println!("Hello, world!");
+    let entered_pass = get_pass().expect("An error occured while reading the password");
+
+    save_pass(&entered_pass).unwrap_or_else(|err| {
+        eprintln!("Problem saving the password: {err}");
+        std::process::exit(1)
+    });
 }
