04332c86925456639df2d1454d3dc466ebd899bd
Koray Aydemir <korayaydemir276@gmail.com>
2023-02-15 01:44:11 +0300

    fix: error handling

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@ mod tests {
         use crate::read_saved_pass;
         use crate::save_pass;
 
-        save_pass("testpass").unwrap();
+        save_pass("testpass");
         let saved_pass = read_saved_pass().unwrap();
 
         assert_eq!(saved_pass, "testpass");
@@ -18,7 +18,7 @@ mod tests {
 
     #[test]
     fn check_pass() {
-        use crate::is_pass_right;
+        use crate::is_pass_correct;
         use crate::Config;
 
         let correct_pass = Config {
@@ -28,11 +28,11 @@ mod tests {
 
         let incorrect_pass = Config {
             saved_pass: "koray_123!*".to_string(),
-            entered_pass: "ay_123!*".to_string(),
+            entered_pass: "bambambam".to_string(),
         };
 
-        assert_eq!(is_pass_right(correct_pass), true);
-        assert_ne!(is_pass_right(incorrect_pass), true);
+        assert_eq!(is_pass_correct(correct_pass), true);
+        assert_ne!(is_pass_correct(incorrect_pass), true);
     }
 }
 
@@ -40,19 +40,22 @@ pub struct Config {
     saved_pass: String,
     pub entered_pass: String,
 }
+
 enum ConfigKind {
     Save,
     DontSave,
 }
 
-pub fn run() {
+pub fn run() -> Result<()> {
     if let Err(_) = read_saved_pass() {
         println!("You don't have a saved pass enter a new one");
-        Config::build(ConfigKind::Save).unwrap();
+        Config::build(ConfigKind::Save)?;
+        Ok(())
     } else {
-        let config = Config::build(ConfigKind::DontSave).unwrap();
-        if is_pass_right(config) {
-            println!("you have gained access")
+        let config = Config::build(ConfigKind::DontSave)?;
+        if is_pass_correct(config) {
+            println!("you have gained access");
+            Ok(())
         } else {
             println!("wrong password, exiting program");
             std::process::exit(0);
@@ -62,10 +65,10 @@ pub fn run() {
 
 impl Config {
     fn build(is_save: ConfigKind) -> Result<Config> {
-        let entered_pass = enter_pass()?;
+        let entered_pass = enter_pass();
         let saved_pass = match is_save {
             ConfigKind::Save => {
-                save_pass(&entered_pass)?;
+                save_pass(&entered_pass);
                 entered_pass.clone()
             }
             ConfigKind::DontSave => read_saved_pass()?,
@@ -81,20 +84,21 @@ impl Config {
 fn read_saved_pass() -> Result<String> {
     fs::read_to_string("saved_pass.txt")
 }
-pub fn save_pass(entered_pass: &str) -> Result<&str> {
-    let mut file = File::create("saved_pass.txt")?;
-    file.write_all(entered_pass.as_bytes())?;
-    Ok("your password has been saved !")
+pub fn save_pass(entered_pass: &str) -> &str {
+    let mut file = File::create("saved_pass.txt").expect("could not create the save file");
+    file.write_all(entered_pass.as_bytes())
+        .expect("could not write to save file");
+    "your password has been saved !"
 }
 
-pub fn enter_pass() -> Result<String> {
+pub fn enter_pass() -> String {
     let mut input = String::new();
-    io::stdin().read_line(&mut input)?;
+    io::stdin().read_line(&mut input).expect("Can't get input");
     let _ = stdout().flush();
-    Ok(input)
+    input
 }
 
-pub fn is_pass_right(config: Config) -> bool {
+pub fn is_pass_correct(config: Config) -> bool {
     if config.entered_pass == config.saved_pass {
         return true;
     } else {
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
 use rust_pass_practice::run;
 fn main() {
-    run();
-
-    //  let entered_pass = get_pass().expect("An error occured while reading the password");
-
-    //  save_pass(&entered_pass).unwrap_or_else(|err| {
-    //      eprintln!("Problem saving the password: {err}");
-    //      std::process::exit(1)
-    //  });
+    if let Err(e) = run() {
+        eprintln!("Could not read saved password: {e}")
+    };
 }
